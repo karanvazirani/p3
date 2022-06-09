ee3d7ede35825f89334c4c23afa6d4a677b85c95
karanvazirani <105892451+karanvazirani@users.noreply.github.com>
2022-06-08 20:39:06 -0700

    Create module.js

---
+++ module.js
@@ -0,0 +1,35 @@
+function validDenomination(coin) {
+    return [1, 5, 10, 25, 50, 100].indexOf(coin) !== -1;
+  }
+  function valueFromCoinObject(obj) {
+
+    const {
+      denom = 0,
+      count = 0,
+    } = obj;
+
+    return denom * count;
+  }
+  function valueFromArray(arr) {
+
+    if (Array.isArray(arr[0])) {
+      arr = arr[0];
+    }
+
+    return arr.reduce(
+      (accumulator, currentObj) =>
+      accumulator + valueFromCoinObject(currentObj),
+      0);
+  }
+  function coinCount(...coinage) {
+
+    return valueFromArray(coinage);
+  }
+
+console.log("{}", coinCount({denom: 5, count: 3}));
+console.log("{}s", coinCount({denom: 5, count: 3},{denom: 10, count: 2}));
+const coins = [{denom: 25, count: 2},{denom: 1, count: 7}];
+console.log("...[{}]", coinCount(...coins));
+console.log("[{}]", coinCount(coins)); 
+
+module.exports = {coinCount};
